f2e08f32f415db7306a5df743bf0378ce0c763d5
Richard Duivenvoorde <richard@duif.net>
2020-04-30 13:06:46 +0200

    Work with one dir per map item (instead of separate sld and map file dirs)

--- src/rodospy/settings.py
+++ src/rodospy/settings.py
@@ -1,21 +1,21 @@
 rodospy_settings = {
     "wps": {
-        # URL for Geoserver WPS service
+        # URL for Geoserver WPS service to retrieve JRodos runs etc
         # it's enough to change host and port
         # "url": "http://localhost:8080/geoserver/wps",
         "url": "http://jrodos.dev.cal-net.nl/geoserver/wps",
         # Local storage of GeoPackage files, must be writeable
         # The directory will be created if it does not exist.
-        "file_storage": "/usr/lib/cgi-bin",
+        "file_storage": "/var/www/mapserver",
         # Local store of sld files, must be writable
         # Path will be extended with a 'scenario'-part (based on time etc)
         # Path should be part of webdir of a webserver so it can be retrieved over http
-        "sld_storage": "/var/www/html/sld",
+        "sld_storage": "/var/www/mapserver",
         # Base url to be used for the sld's (to be used by mapserver as custom sld)
-        "sld_base_url": 'http://localhost/sld',
+        "sld_base_url": 'http://localhost/viewer',
         # FULL wms server url (mapfile-pathname will be appanded to this)
         # used in map items for WMS-T server
-        "wms_server_url": 'http://localhost/cgi-bin/mapserv?map='
+        "wms_server_url": 'http://localhost/mapserver/mapserv?map='
     },
     "rest": {
         # TOMCAT rest service URL
